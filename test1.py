10a0a633589eeb90fa69e6178aed84d70e3337c3
lifelessdragon <125785988+lifelessdragon@users.noreply.github.com>
2023-11-05 15:01:39 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the number a:- "))
+b = int(input("Enter the number b:- "))
+
+
+if b > a :
+    print(" b is graeter than a")
+
+elif a > b :
+    print("a is graeter than b")
+
+elif a == b :
+    print("'a' and 'b' is equal")
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function (fname):
+    print(fname + "Refsnes")
+
+my_function("Email")
+my_function("Tobais")
+my_function("linus")
